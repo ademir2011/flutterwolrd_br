f7ce8ac963512a25552b50bdb27585e935c61a28
Ademir Bezerra <ademirbezerra2012@gmail.com>
2020-02-06 17:38:01 -0300

    projeto inicial com modular e mobx

---
+++ lib/app/app_module.dart
@@ -0,0 +1,26 @@
+import 'package:flutterwolrd_br/app/modules/login/login_module.dart';
+import 'package:flutterwolrd_br/app/pages/splash/splash_controller.dart';
+import 'package:flutterwolrd_br/app/app_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterwolrd_br/app/app_widget.dart';
+import 'package:flutterwolrd_br/app/pages/splash/splash_page.dart';
+
+class AppModule extends MainModule {
+  @override
+  List<Bind> get binds => [
+        Bind((i) => SplashController()),
+        Bind((i) => AppController()),
+      ];
+
+  @override
+  List<Router> get routers => [
+        Router('/', child: (_, args) => SplashPage()),
+        Router('/login', module: LoginModule()),
+      ];
+
+  @override
+  Widget get bootstrap => AppWidget();
+
+  static Inject get to => Inject<AppModule>.of();
+}
