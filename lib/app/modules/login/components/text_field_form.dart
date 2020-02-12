d0b208974f0a28fb49ec6abbda1e2c952352022f
Ademir Bezerra <ademirbezerra2012@gmail.com>
2020-02-12 16:52:16 -0300

    first2

--- lib/app/modules/login/components/text_field_form.dart
+++ lib/app/modules/login/components/text_field_form.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+// import 'package:flutterwolrd_br/app/utils/generate_profile.dart';
 
 class Text_Field_Form {
   static Widget textFieldForm(
           {String labelText, onChanged, String Function() validateString}) =>
       TextFormField(
+        // initialValue: labelText == 'login'
+        //     ? GenerateProfile.login()
+        //     : GenerateProfile.password(),
         onChanged: onChanged,
         decoration: InputDecoration(
           labelText: labelText,
